af92ae8d1f9c37f5af727b679087ca4f55fee0d2
Rishi Pandey <rishipandeyinfo@gmail.com>
2024-04-25 21:16:52 +0530

    Project Completed

--- Section 1 - Passenger Counter App/index.js
+++ Section 1 - Passenger Counter App/index.js
@@ -1,6 +1,7 @@
 //  document.getElementById("count-el").innerText= 5;
 
 let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 
 
 let count = 0;
@@ -14,3 +15,19 @@ function decrement(){
     count--;
     countEl.innerText = count;
 }
+
+function save(){
+let countStr = count + " ,";
+
+saveEl.textContent += countStr;
+countEl.textContent = 0;
+count = 0;
+}
+
+
+
+// Concepts
+// let username = "Rishi"
+// let para = "You have three new notification"
+// let final = `${username} ${para}`;
+// console.log(final);
